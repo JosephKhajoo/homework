4d4a825ff75d51f8a7819ff2ce4dad2b2ca6d263
JosephKhajoo <36998069+JosephKhajoo@users.noreply.github.com>
2021-01-24 23:53:58 +0400

    Update download_images.py

--- download_json/download_images.py
+++ download_json/download_images.py
@@ -1,25 +1,24 @@
+from fill_json import FillJson
 from concurrent.futures import ThreadPoolExecutor
 import logging
 import requests
 import json
 import os
 
-datefmt = "%d-%m-%Y %I:%H:%S"
-logging.basicConfig(level=logging.DEBUG, filename="events.log", filemode="w", format="%(levelname)s: %(asctime)s - %(message)s", datefmt=datefmt)
-
 
 class ImageDownload():
-	def __init__(self, json_file):
-		self.json_file = json_file
+	def __init__(self, web_link):
+		self.web_link = web_link
+
 		if not os.path.exists("images"):
 			logging.info("Creating folder 'images'.")
 			os.mkdir("images")
-		try:
-			with open(json_file, "r") as file:
-				self.data = json.load(file)["images"]
-				logging.debug(f"Data from json file inside 'images'\n {self.data} {type(self.data)}")
-		except FileNotFoundError:
-			logging.error(f"File `{self.json_file}` was not found", exc_info=True)
+		
+		a = FillJson(self.web_link)
+		a.fill()
+
+		with open("images.json", 'r') as file:
+			self.data = json.load(file)["images"]
 
 	def download(self, items):
 		pic = requests.get(items["link"])
@@ -31,6 +30,7 @@ class ImageDownload():
 	def handler(self):
 		try:
 		    with ThreadPoolExecutor(max_workers=len(self.data)) as executor:
+		    	logging.debug(self.data)
 		    	logging.debug(f"{len(self.data)} threads created - {executor}")
 		    	executor.map(self.download, self.data)
 		except AttributeError:
@@ -39,5 +39,5 @@ class ImageDownload():
 		    logging.info("Done.")
 		    print("\nDone. ")
 
-img1 = ImageDownload("images.json")
+img1 = ImageDownload("https://www.xkcd.com/")
 img1.handler()
