653165df440d8bea6325341d97de9b08f9c86452
JosephKhajoo <josephkhajo2003@gmail.com>
2021-01-29 10:58:39 -0500

    decorator homework

---
+++ decorator_homework.py
@@ -0,0 +1,26 @@
+import logging
+import time
+
+def function_timer(filename="output.txt") -> callable:
+	def wrapper(func: callable) -> callable:
+		def decor(*args):
+			start = time.time()
+			result = func(*args)
+			finish = time.time() - start
+			try:
+				with open(filename, "w") as file:
+					file.write(f"{result}\n\nFinished in {finish} seconds.")
+			except Exception as e:
+				logging.error(f"An error occurred in `decor` function - {e}")
+
+			print(f"Finished in {finish} seconds")
+		return decor
+	return wrapper
+
+
+@function_timer()
+def my_func(num):
+	a = [i ** 4 for i in range(num)]
+	return a
+
+my_func(1000)
